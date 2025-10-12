2db4eeba7ce53aef9d30a124adbfd3f0788a094d
dealbertt <dealbertignacio@gmail.com>
2025-10-12 23:10:04 +0200

    Bit of progress, mostly on the config side

--- src/main.zig
+++ src/main.zig
@@ -2,9 +2,11 @@ const std = @import("std");
 const rl = @import("raylib");
 const utils = @import("z80.zig");
 const config = @import("config.zig");
+const print = @import("std").debug.print;
 
 //Todo on main:
 //- Initialize raylib
+//- Load the config
 //- Get the path to the rom or program through a command line argument
 //
 //
@@ -12,22 +14,22 @@ const config = @import("config.zig");
 //
 
 pub fn main() !void {
-    const emulConfig = config.emulConfig { .width= 1280, .height= 720, .debug = false, .fps = 60 };
-    //const emulConfig = config.loadConfig();
+    //const emulConfig = config.emulConfig { .width= 1280, .height= 720, .debug = false, .fps = 60 };
+    const cfg = try config.loadConfig();
 
-    rl.initWindow(emulConfig.width, emulConfig.height, "ZxSpectrum emulator"); 
+    rl.initWindow(cfg.width, cfg.height, "ZxSpectrum emulator"); 
     defer rl.closeWindow();
 
-    rl.setTargetFPS(emulConfig.fps);
+    rl.setTargetFPS(cfg.fps);
 
-    emulConfig.reportConfig();
+    
     while(!rl.windowShouldClose()){
 
         rl.beginDrawing();
         defer rl.endDrawing();
 
         rl.clearBackground(.white);
-        rl.drawText("Welcome to the ZXSpectrum emulator", emulConfig.width / 2, emulConfig.height / 2, 20, .red);
+        rl.drawText("Welcome to the ZXSpectrum emulator", cfg.width / 2, cfg.height / 2, 20, .red);
     }
 }
 
--- src/zxspectrum.zig
+++ src/zxspectrum.zig
@@ -3,5 +3,6 @@ const std = @import("std");
 const screenWidth: u8 = 256;
 const screenHeight: u8 = 192;
 
+var gpx: [screenWidth][screenHeight]u8 = 0;
 var keyPad: u8[40] = [_]u8{0} ** 40;
 
