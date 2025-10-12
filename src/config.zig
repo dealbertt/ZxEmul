f3fd97d8cde39da7135a024682a45a0088b9ba7e
dealbertt <dealbertignacio@gmail.com>
2025-10-12 23:09:38 +0200

    Finally got some sort of file reading going on

--- src/config.zig
+++ src/config.zig
@@ -1,7 +1,11 @@
 const std = @import("std");
+const print = @import("std").debug.print;
 const cwd = std.fs.cwd();
 
-const configPath = "config/config.txt";
+const c = @cImport(@cInclude("stdio.h"));
+
+
+const configPath = "data/config.txt";
 
 pub const emulConfig = struct{
     height: u16,
@@ -9,25 +13,52 @@ pub const emulConfig = struct{
     debug: bool,
     fps: u8,
     pub fn reportConfig(self: emulConfig) void{
-        std.debug.print("Window Width: {}\n", .{self.width});
-        std.debug.print("Window Height: {}\n", .{self.height});
-        std.debug.print("FPS: {}\n", .{self.fps});
+        print("Window Width: {}\n", .{self.width});
+        print("Window Height: {}\n", .{self.height});
+        print("FPS: {}\n", .{self.fps});
+        print("Debug: {}\n", .{self.debug});
     }
     
 };
 
-
 pub fn loadConfig() !emulConfig{
-    var file = try cwd.openFile(configPath, .{});
-    defer file.close();
 
-    var bufReader = std.io.bufferedReader(file.reader());
-    var inStream = bufReader.reader();
+    //We open the file only to read it
+    //var file = try cwd.openFile(configPath, .{.mode = .read_only});
 
-    var buf: [1024]u8 = undefined;
-    while (try inStream.readUntilDelimiterOrEof(&buf, '\n')) |line| {
-        std.debug.print("Line: {s}\n", .{line});
+    const file = c.fopen(configPath, "r");
+
+    if(file == null){
+        return error.FileNotFound;
+    }
+    var line: [100]u8 = undefined;
+
+    //var buf: [256]u8 = undefined;
+    //var reader = file.reader(&buf).interface;
+
+    //we declare a default config (until i can change the values from the config file, this tays as a const)
+    const cfg: emulConfig = emulConfig{
+        .width = 1280,
+        .height = 720,
+        .debug = false,
+        .fps = 60,
+    };
+
+    //given that in C, an array is pointer to the first element, we quite literally have to do that in Zig.
+    //
+    while(c.fgets(&line[0], line.len , file) != null){
+        var end: usize = 0;
+        while(end < line.len and line[end] != 0): (end += 1){}
+
+        const slice = line[0..end];
+        const trimmed = std.mem.trim(u8, slice, "\t\r\n");
+        
+        print("Line: {s}\n", .{trimmed});
+        if (trimmed.len == 0 or trimmed[0] == '#') continue;
+
     }
 
-    return null;
+
+    cfg.reportConfig();
+    return cfg;
 }
