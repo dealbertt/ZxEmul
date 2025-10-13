ed22fa78667d29c9c2a5a9235d7717d58eb124eb
dealbertt <dealbertignacio@gmail.com>
2025-10-13 10:42:51 +0200

    Got the config file working

--- src/config.zig
+++ src/config.zig
@@ -7,7 +7,7 @@ const c = @cImport(@cInclude("stdio.h"));
 
 const configPath = "data/config.txt";
 
-pub const emulConfig = struct{
+const emulConfig = struct{
     height: u16,
     width: u16,
     debug: bool,
@@ -37,7 +37,7 @@ pub fn loadConfig() !emulConfig{
     //var reader = file.reader(&buf).interface;
 
     //we declare a default config (until i can change the values from the config file, this tays as a const)
-    const cfg: emulConfig = emulConfig{
+    var cfg: emulConfig = emulConfig{
         .width = 1280,
         .height = 720,
         .debug = false,
@@ -53,8 +53,29 @@ pub fn loadConfig() !emulConfig{
         const slice = line[0..end];
         const trimmed = std.mem.trim(u8, slice, "\t\r\n");
         
-        print("Line: {s}\n", .{trimmed});
+        //Skip empty lines or comments
         if (trimmed.len == 0 or trimmed[0] == '#') continue;
+
+        //We split the values on the = sign 
+        var splitter = std.mem.splitAny(u8, trimmed, "=");
+        
+        //we get the key
+        const key = splitter.first();
+
+        //we get the value
+        const value = splitter.rest();
+
+        if(std.mem.eql(u8, key, "WINDOW_WIDTH")){
+            cfg.width = try std.fmt.parseInt(u16, value, 10);
+            if(cfg.width > 1920) cfg.width = 1920;
+
+        }else if(std.mem.eql(u8, key, "WINDOW_HEIGHT")){
+            cfg.height= try std.fmt.parseInt(u16, value, 10);
+            if(cfg.height > 1080) cfg.height = 1080;
+        }else if(std.mem.eql(u8, key, "REFRESH_RATE")){
+            cfg.fps = try std.fmt.parseInt(u8, value, 10);
+            if(cfg.fps > 240) cfg.fps = 60;
+        }
 
     }
 
