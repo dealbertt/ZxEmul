036eec427da93b6e64ad0c57202a319277a3c030
dealbertt <ignaciodealbertmartin@gmail.com>
2026-09-08 11:37:12 +0200

    fetch_byte was incrementing the pc twice

--- src/z80/internals/execution.zig
+++ src/z80/internals/execution.zig
@@ -6,10 +6,6 @@ const t = @import("../instructions/tables.zig");
 
 pub fn fetch_byte(state: *s.State) u8 {
     const byte = mem.read8(state, &state.pc);
-
-    //increase pc, avoiding overflow with %
-    state.pc +%= 1;
-
     //std.debug.print("Current opcode {} \n", .{state.opcode});
 
     return byte;
